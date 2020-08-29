0ed08190348ffbc4f1aaf674c9a89b6cb8f1fee8
ryuv <sujea777@naver.com>
2020-08-29 16:25:09 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
+  @import url(https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css);
+  @import url('https://cdn.rawgit.com/innks/NanumSquareRound/master/nanumsquareround.min.css');
+  @font-face { font-family: 'TmonMonsori'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/TmonMonsori.woff') format('woff'); font-weight: normal; font-style: normal; }
+/* Css init */
+html{
+  width:100%;
+}
+body {
+  background-color: #1F2124 !important;
+  word-break: keep-all;
+  margin: 0;
+  padding: 0; 
+  width:100%;
+  color:#333;
+  /* font-family: 'NanumSquare', sans-serif !important; */
+}
+input {
+  -webkit-appearance: none;
+  -webkit-border-radius: 0;
 }
 
-#nav {
-  padding: 30px;
+p {
+  margin: 0px !important;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* Css init */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,256 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <div style="width:100%; height:60px; background-image:url('https://i.ibb.co/cDs53mf/2c119ae830b0ee08e629b2f828a71eb1-1519264118-06.jpg'); background-size:cover; background-position:40px 190px;">
+      <!-- Header -->
+      <div style="width:100%; height:100%; background-color:rgba(0,0,0,0.5); padding:20px;" class="d-flex justify-content-start align-items-center">
+        <span class="logo">Baram.zz</span>
+      </div>
+      <!-- Header -->
+<!-- 
+      <div style="width:100%; height:300px;padding:20px; background-color:#fff;">
+        <p style="font-size:16px; font-weight:bold;">보스 젠시간 찾기</p>
+        <div style="margin-top:10px;" class="d-flex justify-content-center align-items-start">
+          <img src="https://i.ibb.co/k0pmzJX/image.png" style="width:30%; margin-right:20px;">
+
+
+          <div style="width:70%; padding: 10px 0; font">
+            <p style="font-size:12px; color:#999;">연 서버</p>
+            <p style="font-size:14px; font-weight:500;">다음 젠 시간 : 20:37</p>
+          </div>
+        </div>
+
+      </div> -->
+
+      <div style="padding:20px 20px 50px; background-color:#1F2124;">
+        <p style="font-size:16px; font-weight:bold; color:#fff;">보스 젠시간 찾기</p>
+        
+        <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/Gv8NCFP/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">백호왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+          <div style="width:48%; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/j4vMWVy/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">적호왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/XD0Hpn5/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">도깨비왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+          <div style="width:48%; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/x5sdTsK/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">일본밀정두목</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/xhFhPqr/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">인형술사</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+          <div style="width:48%; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/FWFWC2Q/2.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">사마귀왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/MkR6sQL/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">전갈왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+          <div style="width:48%; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/m579y4C/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">팔괘: 쥐왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/LvrM9XS/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">팔괘: 돼지왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+          <div style="width:48%; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/ftm9SNp/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">팔괘: 닭왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+        </div>
+
+         <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/rZprZJS/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;"> 팔괘: 양왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+          <div style="width:48%; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/4ZSk2q8/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">팔괘: 원숭이왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+        </div>
+
+         <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/XZNTX6J/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">팔괘: 뱀왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+          <div style="width:48%; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/Gv8NCFP/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">팔괘: 백호왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-between" style="margin-top:10px;">
+          <div style="width:48%; margin-right:10px; border:1px solid #754F44; padding:10px 0; border-radius:5px; background-color:#edcb95">
+            <div style="text-align:center; padding-bottom:10px; border-bottom:1px solid #754F44;">
+             <img src="https://i.ibb.co/4P2rXvQ/image.png" style="width:80%;">
+              <p style="font-size:14px; font-weight:bold; padding-top:5px;">팔괘: 토끼왕</p>
+            </div>
+
+            <div style="font-size:14px; font-weight:500; padding-top:10px; text-align:center;">
+              <p>다음 젠 시간</p>
+              <p style="font-size:16px;">20:18</p>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+      <!-- Footer -->
+      <div style="width:100%; background-color:#383A3F; padding: 20px; color:#a0a0a0; font-size:12px;">
+        <p>© Baram.zz. All Rights Reserved. </p>
+        <p>block@kakao.com</p>
+
+        <p style="padding-top:10px;">헤당 홈페이지는 베타서비스 중이며 불편한 점과 추가되었으면 하는 서비스가있다면 메일로 의견주시면 감사하겠습니다 :)</p>
+      </div>
+      <!-- Footer -->
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
   }
 };
 </script>
+
+<style>
+  .logo {
+    font-family: 'Russo One', sans-serif;
+    color:#fff;
+    font-size:20px;
+  }
+</style>
